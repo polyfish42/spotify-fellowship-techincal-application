9f4347005bf9b99b5644162b330c452e9173d902
Jacob Brady <polyfish42@gmail.com>
2018-06-21 20:47:43 -0400

    Add actions to view controller

--- CalendarMoblie/CalendarMoblie/ViewController.swift
+++ CalendarMoblie/CalendarMoblie/ViewController.swift
@@ -13,53 +13,86 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     //MARK: Properties
     @IBOutlet weak var eventTitle: UITextField!
-    
     @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    
+    struct CalendarEvent: Encodable {
+        var title: String
+        var startDate: Date
+        var endDate: Date
+        var description: String
+        
+        enum CodingKeys: String, CodingKey {
+            case event
+        }
+        
+        enum Event: String, CodingKey {
+            case title
+            case startDate = "start_date"
+            case endDate = "end_date"
+            case description
+        }
+        
+        func encode(to encoder: Encoder) throws {
+            var container = encoder.container(keyedBy: CodingKeys.self)
+            var event = container.nestedContainer(keyedBy: Event.self, forKey: .event)
+            try event.encode(title, forKey: .title)
+            try event.encode(startDate, forKey: .startDate)
+            try event.encode(endDate, forKey: .endDate)
+            try event.encode(description, forKey: .description)
+        }
+    }
+    
+    var calEvent = CalendarEvent(title: "", startDate: Date.init(), endDate: Date.init(), description: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         eventTitle.delegate = self
         startDatePicker.isHidden = true
+        endDatePicker.isHidden = true
     }
     
     //MARK: UITextFieldDelegate
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        eventTitle.resignFirstResponder()
+        textField.resignFirstResponder()
         return true
+    }
+    
+    @IBAction func titleDidEndEditing(_ sender: UITextField) {
+        calEvent.title = sender.text!
+    }
+    
+    @IBAction func descriptionDidEndEditing(_ sender: UITextField) {
+        calEvent.description = sender.text!
     }
     
     //MARK: Actions
     
     @IBAction func tapStartDate(_ sender: UIButton) {
-        startDatePicker.isHidden = false
+        if startDatePicker.isHidden {
+            startDatePicker.isHidden = false
+            endDatePicker.isHidden = true
+        } else {
+            startDatePicker.isHidden = true
+        }
+    }
+    
+    @IBAction func tapEndDate(_ sender: UIButton) {
+        if endDatePicker.isHidden {
+            endDatePicker.isHidden = false
+            startDatePicker.isHidden = true
+        } else {
+            endDatePicker.isHidden = true
+        }
+    }
+    
+    @IBAction func startDatePicked(_ sender: UIDatePicker) {
+        let date = startDatePicker.date
+        print(date)
     }
     
     @IBAction func submitEvent(_ sender: UIButton) {
-        struct CalendarEvent: Encodable {
-            var title: String
-            var description: String
-            
-            enum CodingKeys: String, CodingKey {
-                case event
-            }
-            
-            enum Event: String, CodingKey {
-                case title
-                case description
-            }
-            
-            func encode(to encoder: Encoder) throws {
-                var container = encoder.container(keyedBy: CodingKeys.self)
-                var event = container.nestedContainer(keyedBy: Event.self, forKey: .event)
-                try event.encode(title, forKey: .title)
-                try event.encode(description, forKey: .description)
-            }
-        }
-        
-        let calEvent = CalendarEvent(title: "Developer Week NYC", description: "Free drinks!")
-        
-        
         guard let uploadData = try? JSONEncoder().encode(calEvent) else {
             return
         }
