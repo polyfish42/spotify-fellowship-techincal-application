a3e9139281e57d6ee29c4cd26a57ac8a21401f9f
Jacob Brady <polyfish42@gmail.com>
2018-06-25 17:41:38 -0400

    Add basic collection view for calendar

---
+++ CalendarMoblie/CalendarMoblie/Controllers/CalendarViewDay.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarViewDay.swift
+//  CalendarMoblie
+//
+//  Created by Jacob Brady on 6/25/18.
+//  Copyright © 2018 Jacob Brady. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewDay: UICollectionViewCell {
+    @IBOutlet weak var dayNumberLabel: UILabel!
+}
--- CalendarMoblie/CalendarMoblie/Controllers/HomeViewController.swift
+++ CalendarMoblie/CalendarMoblie/Controllers/HomeViewController.swift
@@ -8,8 +8,38 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITextFieldDelegate {
+class HomeViewController: UIViewController, UICollectionViewDataSource, UITextFieldDelegate {
     //MARK: Model
+    let days = ["1",
+                "2",
+                "3",
+                "4",
+                "5",
+                "6",
+                "7",
+                "8",
+                "9",
+                "10",
+                "11",
+                "12",
+                "13",
+                "14",
+                "15",
+                "16",
+                "17",
+                "18",
+                "19",
+                "20",
+                "21",
+                "22",
+                "23",
+                "24",
+                "25",
+                "26",
+                "27",
+                "28",
+                "29",
+                "30"]
     
     //MARK: Properties
     
@@ -17,7 +47,18 @@ class HomeViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return days.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        print("view loaded")
+        let day = collectionView.dequeueReusableCell(withReuseIdentifier: "Day", for: indexPath) as! CalendarViewDay
+        
+        day.dayNumberLabel.text = days[indexPath.item]
+        
+        return day
     }
 }
