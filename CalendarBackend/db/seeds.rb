30149a49b88e521bd049354d603bb0806d4f2aef
Jacob Brady <polyfish42@gmail.com>
2018-06-27 20:23:30 -0400

    Add seed file

--- CalendarBackend/db/seeds.rb
+++ CalendarBackend/db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+Event.destroy_all
+
+now = DateTime.now
+yesterday = DateTime.now - 1
+tomorrow = DateTime.now + 1
+next_day = DateTime.now + 2
+Event.create!([
+    {title: "Run", start_date: yesterday.change({hour: 7, min: 0, sec: 0}), end_date: yesterday.change({hour: 8, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Wilco Concert", start_date: yesterday.change({hour: 19, min: 0, sec: 0}), end_date: yesterday.change({hour: 21, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Go to the gym", start_date: now.change({hour: 7, min: 0, sec: 0}), end_date: now.change({hour: 8, min: 30, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Listen to Spotify", start_date: now.change({hour: 8, min: 30, sec: 0}), end_date: now.change({hour: 9, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Build App", start_date: now.change({hour: 9, min: 0, sec: 0}), end_date: now.change({hour: 12, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Lunch with Jamie", start_date: now.change({hour: 12, min: 30, sec: 0}), end_date: now.change({hour: 13, min: 30, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "AWS Meetup", start_date: now.change({hour: 18, min: 30, sec: 0}), end_date: now.change({hour: 19, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "3 Day GOT Marathon", start_date: now.change({hour: 20, min: 0, sec: 0}), end_date: next_day.change({hour: 8, min: 30, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Run", start_date: tomorrow.change({hour: 7, min: 0, sec: 0}), end_date: tomorrow.change({hour: 8, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Coffee with Amanda", start_date: tomorrow.change({hour: 8, min: 30, sec: 0}), end_date: tomorrow.change({hour: 9, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Call Mom", start_date: tomorrow.change({hour: 19, min: 0, sec: 0}), end_date: tomorrow.change({hour: 20, min: 0, sec: 0}), description: Faker::Lorem.characters(40)},
+    {title: "Go to the Gym", start_date: next_day.change({hour: 7, min: 0, sec: 0}), end_date: next_day.change({hour: 8, min: 0, sec: 0}), description: Faker::Lorem.characters(40)}
+])
